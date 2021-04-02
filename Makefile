8788547444672a5f228c188991fd629580ff8763
linus345 <linusekman01@gmail.com>
2021-04-02 14:26:51 +0200

    makefile för linux och Windows

---
+++ Makefile
@@ -0,0 +1,31 @@
+# set the compiler
+CC := gcc
+
+# general compiler flags
+CFLAGS := --std=c99 -Wall -Wextra -pedantic
+
+ifeq ($(OS),Windows_NT)
+	CFLAGS += -IC:\libsdl\include -LC:\libsdl\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_net -lSDL2_image
+	EXEC := snek.exe
+else
+	UNAME_S := $(shell uname -s)
+	ifeq ($(UNAME_S),Linux)
+		CFLAGS += `sdl2-config --libs --cflags` -lSDL2_net -lSDL2_image
+		EXEC := snek.out
+	endif
+	ifeq ($(UNAME_S),Darwin)
+		CFLAGS += `sdl2-config --libs --cflags` -lSDL2_net -lSDL2_image
+		EXEC := snek.out
+	endif
+endif
+
+$(EXEC): main.o
+	$(CC) *.o $(CFLAGS) -o $(EXEC)
+
+main.o: main.c
+	$(CC) main.c -c $(CFLAGS)
+
+clean:
+	rm -f $(EXEC) *.o
+
+.PHONY: clean
---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_net.h>
+#include <SDL2/SDL_image.h>
+
+int main(int argc, char *argv[])
+{
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error: SDL_Init: %s\n", SDL_GetError());
+        return 1;
+    }
+    printf("successfully initialized SDL\n");
+    if(SDLNet_Init() != 0) {
+        fprintf(stderr, "Error: SDLNet_Init: %s\n", SDLNet_GetError());
+        return 2;
+    }
+    printf("successfully initialized SDL_net\n");
+
+    SDL_Surface *surface = IMG_Load("../snake-test/textures/snakehead.png");
+    if(!surface) {
+        fprintf(stderr, "error creating surface\n");
+        return 3;
+    }
+    printf("SDL_image works\n");
+
+    SDLNet_Quit();
+    SDL_Quit();
+    return 0;
+}
